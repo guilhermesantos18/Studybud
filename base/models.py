a6638a105702987019aee54cd5418bdabdde277f
Guilherme Santos <guipinela.santos@gmail.com>
2022-01-21 12:45:15 +0000

    commit

--- base/models.py
+++ base/models.py
@@ -9,6 +9,7 @@ class Topic(models.Model):
         return self.name
 
 
+
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
@@ -17,6 +18,9 @@ class Room(models.Model):
     #participants
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return str(self.name)
