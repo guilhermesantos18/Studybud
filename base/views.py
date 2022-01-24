46f6cd945c23c4361b6062f2e57d5a383d55356d
Guilherme Santos <guipinela.santos@gmail.com>
2022-01-24 16:38:00 +0000

    commit
    commit

--- base/views.py
+++ base/views.py
@@ -78,6 +78,14 @@ def home(request):
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
 
 def room(request, pk):
 
